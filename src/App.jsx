82d1fff5aaf60fbdeed686994b9cc4811cf7e7c7
Dulith Mutucumarana <dulithsasin@gmail.com>
2026-05-20 17:17:48 +0530

    feat: init app

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', marginTop: '4rem' }}>
+      <h1>Test App</h1>
+      <p>GitHub pipeline test bundle</p>
+    </div>
+  )
+}
